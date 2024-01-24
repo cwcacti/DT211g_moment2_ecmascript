2b642c264595d000cf466fdf34b04a8e0d715303
Filip Nöbelin <filip.nobelin@gmail.com>
2024-01-24 15:55:43 +0100

    Lägg till uppstartsfunktion till js
    Lagt till window.onload och startUp, även en variabel för sökrutan

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,11 @@
 /* Variabler för existerande HTML-element */
 const mainDivElement = document.getElementById("innehåll")
 const searchBoxElement = document.getElementById("sökruta")
+
+/* Åkallar uppstartsfunktion */
+window.onload = (startUp);
+
+/*Uppstartsfunktion */
+function startUp() {
+
+}
