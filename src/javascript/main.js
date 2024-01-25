78202fa43959cdd1e455054178c7678b31daed89
Filip Nöbelin <filip.nobelin@gmail.com>
2024-01-25 18:41:07 +0100

    Lägg in grundfunktionalitet i js
    Lagt till tableelement i index.html
    Skapat funktioner för att skapa grundtabellen i html mha javascript
    Nästa steg: gör listan sök och sorterbar
    Detta kräver kanske att listan görs om till egen array med objekt

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,5 +1,5 @@
 /* Variabler för existerande HTML-element */
-const mainDivElement = document.getElementById("innehåll")
+const mainElement = document.getElementById("innehåll")
 const searchBoxElement = document.getElementById("sökruta")
 
 /* Åkallar uppstartsfunktion */
@@ -8,4 +8,42 @@ window.onload = (startUp);
 /*Uppstartsfunktion */
 function startUp() {
 
+    getData();
+    checkData();
+
+}
+
+/* Hämta data */
+
+async function getData() {
+    const response = await fetch("https://dahlgren.miun.se/ramschema_ht23.php?format=json");
+    if (response.ok) {
+        const data = await response.json();
+        return data;
+    }
+}
+
+/* Skriv ut data i dokumentet */
+async function checkData() {
+    let data = await getData();
+    for (const object of data) {
+
+        let newTrElement = document.createElement("tr");
+        mainElement.appendChild(newTrElement);
+
+        let firstTdElement = document.createElement("td");
+        let firstTdText = document.createTextNode(object.code);
+        newTrElement.appendChild(firstTdElement);
+        firstTdElement.appendChild(firstTdText);
+
+        let secondTdElement = document.createElement("td");
+        let secondTdText = document.createTextNode(object.coursename);
+        newTrElement.appendChild(secondTdElement);
+        secondTdElement.appendChild(secondTdText);
+
+let thirdTdElement = document.createElement("td");
+let thirdTdText = document.createTextNode(object.progression);
+newTrElement.appendChild(thirdTdElement);
+thirdTdElement.appendChild(thirdTdText);
+    }
 }
